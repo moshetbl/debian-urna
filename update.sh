25c1855c9edb1ceab6090bb0d30951b53b55e1e3
Thomaz Reis <thor27@gmail.com>
2016-04-20 23:39:09 -0300

    Suporte ao Keyboard Websocket

--- update.sh
+++ update.sh
@@ -10,6 +10,15 @@ cp ../multimonitor-browser/browser.py root/usr/local/bin/browser
 chmod +x root/usr/local/bin/browser
 cp ../multimonitor-browser/detect_monitors.py root/usr/local/bin/browser-detect-monitors
 chmod +x root/usr/local/bin/browser-detect-monitors
+rm -rf root/opt/keyboard-websocket
+cp -r ../keyboard-websocket root/opt
+mkdir root/opt/keyboard-websocket/packages
+pip install --download root/opt/keyboard-websocket/packages -r root/opt/keyboard-websocket/requirements.txt
+
+#HACK FIX for GIT req
+cp root/opt/keyboard-websocket/requirements.txt root/opt/keyboard-websocket/requirements.txt.original
+cat root/opt/keyboard-websocket/requirements.txt.original | grep -v git > root/opt/keyboard-websocket/requirements.txt
+echo 'socketIO-client==0.6.6' >> root/opt/keyboard-websocket/requirements.txt
 
 cd root
 tar -czf urna_extras.tar.gz *
