69af56ae5476bda667b7baaed0bd1a8379ec452d
Thomaz dos Reis <thor27@gmail.com>
2016-11-11 20:37:03 -0200

    Correção do erro no instalador

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+export udebs_folder=`pwd`/udebs
 cd simple-cdd
 export simple_cdd_dir=`pwd`
-build-simple-cdd --conf ./simple-cdd.conf --graphical-installer -p urna -a urna --locale pt_BR --keyboard br
+build-simple-cdd --local-packages $udebs_folder --conf ./simple-cdd.conf --graphical-installer -p urna -a urna --locale pt_BR --keyboard br
